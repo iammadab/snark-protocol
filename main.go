06fffb628b98956873150b3ef750949c1fddd2b2
Wisdom Ogwu <iammadab@gmail.com>
2022-07-02 16:14:09 +0100

    add test cases for the protocol

--- main.go
+++ main.go
@@ -21,7 +21,8 @@ func NewVerifier(field *field.Field, generator int64, coefficients []int64) *Ver
 		Generator: generator,
 		PolyT:     *polynomial.NewPolynomial(field, coefficients),
 		Field:     field,
-		EvalPoint: field.RandomElement(),
+		// EvalPoint: field.RandomElement(),
+		EvalPoint: 2,
 	}
 }
 
@@ -32,11 +33,13 @@ func (verifier *Verifier) Setup() []int64 {
 	encryptedPowers := []int64{}
 	for i := 0; i <= verifier.PolyT.Degree(); i++ {
 		power := IntPow(verifier.EvalPoint, int64(i))
+		println("powers")
+		println("%v", power)
 		encryptedPowers = append(encryptedPowers, verifier.EncryptValue(power))
 	}
 
 	fmt.Println("encrypted powers of x")
-	fmt.Printf("%v", encryptedPowers)
+	fmt.Printf("%v+", encryptedPowers)
 	return encryptedPowers
 }
 
@@ -63,28 +66,6 @@ func NewProver(field *field.Field, polyp []int64, polyh []int64) *Prover {
 		PolyP: *polynomial.NewPolynomial(field, polyp),
 		PolyH: *polynomial.NewPolynomial(field, polyh),
 	}
-}
-
-func main() {
-	field := field.NewField(7)
-
-	// need a more intiutive way to set the co-efficients
-	// p(x) = x^3 - 3x^2 + 2x [0, 2, -3, 1]
-	// h(x) = x^2 - 2x [0, -2, 1, 0]
-	// t(x) = x - 1 [-1, 1, 0, 0]
-
-	t_of_x := []int64{-1, 1, 0, 0}
-	h_of_x := []int64{0, -2, 1, 0}
-	p_of_x := []int64{0, 2, -3, 1}
-
-	verifier := NewVerifier(field, 5, t_of_x)
-
-	encrypted_powers_of_x := verifier.Setup()
-
-	prover := NewProver(field, p_of_x, h_of_x)
-	fmt.Println(prover)
-	fmt.Println(prover.Prove(encrypted_powers_of_x))
-	println(verifier.Verify(prover.Prove(encrypted_powers_of_x)))
 }
 
 // Helper function to perfom integer exponentiation in golang
---
+++ main_test.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/iammadab/snark-protocol/field"
+)
+
+type TestCase struct {
+	t_of_x, p_of_x, h_of_x []int64
+	is_valid               bool
+}
+
+var testCases = []TestCase{
+	// TODO: write a simple compiler to accept polynomial in written form
+
+	// t(x) = x - 1 [-1, 1, 0, 0]
+	// p(x) = x^3 - 3x^2 + 2x [0, 2, -3, 1]
+	// h(x) = x^2 - 2x [0, -2, 1, 0]
+	{
+		[]int64{-1, 1, 0, 0},
+		[]int64{0, 2, -3, 1},
+		[]int64{0, -2, 1, 0},
+		true,
+	},
+
+	// t(x) = x - 1 [-1, 1, 0, 0]
+	// p(x) = x^3 - 3x^2 + 3x [0, 3, -3, 1]
+	// h(x) = x^2 - 3x [0, -3, 1, 0]
+	{
+		[]int64{-1, 1, 0, 0},
+		[]int64{0, 3, -3, 1},
+		[]int64{0, -3, 1, 0},
+		false,
+	},
+}
+
+func TestProtocol(t *testing.T) {
+	field := field.NewField(7)
+	generator := 5
+
+	for i, test := range testCases {
+		verifier := NewVerifier(field, int64(generator), test.t_of_x)
+
+		encrypted_powers_of_x := verifier.Setup()
+
+		prover := NewProver(field, test.p_of_x, test.h_of_x)
+		p, h := prover.Prove(encrypted_powers_of_x)
+		proofs_validity := verifier.Verify(p, h)
+
+		if proofs_validity != test.is_valid {
+			t.Errorf("Test: %d, expected verifier to say %t, instead got %t", i, test.is_valid, proofs_validity)
+		}
+	}
+}
